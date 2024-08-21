677542d527146a5ca5a3b267c5d23caf43299539
CarsonRDavis <carsondavis340@gmail.com>
2024-08-20 20:22:33 -0400

    Refactored code to follow standard practices and added a Log component to show what turns have been taken.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+export default function GameBoard({ onSelectSquare, turns }) {
 
-    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    // Previous method of updating button on game board.
+    /* const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
@@ -18,7 +26,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
         });
 
         onSelectSquare();
-    }
+    } */
 
     return (
         <ol id="game-board">
@@ -27,7 +35,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
